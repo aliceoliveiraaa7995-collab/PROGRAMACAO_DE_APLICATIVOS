d1e03bdd8fe1d44374a89faf06ace78b2cff6470
Alice Antunes <aliceoliveiraaa7995@gmail.com>
2026-02-27 16:48:41 -0300

    Add files via upload

---
+++ projetoAluno/Aluno.java
@@ -0,0 +1,30 @@
+class Aluno {
+    private String nome;
+    private int idade;
+    private String matricula;
+}
+Aluno (){
+}
+Aluno (String nome, int idade, String matricula){
+    this.nome = nome;
+    this.idade = idade;
+    this.matricula= matricula;
+}
+public String getNome(){
+    return = this.nome;
+}
+public void setNome(){
+    this.nome = nome;
+}
+public int getIdade(){
+    return = this.idade;
+}
+public void setIdade(){
+    this.idade = idade;
+}
+public String getMatricula(){
+    return = this.idade;
+}
+public void setMatricula(){
+    this.matricula = matricula;
+}
