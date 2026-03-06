209b76c9d81d2c0eb80518847d3ccb8978067677
Alice Antunes <aliceoliveiraaa7995@gmail.com>
2026-03-06 16:39:09 -0300

    Update CarroClass.java

--- Carro/CarroClass.java
+++ Carro/CarroClass.java
@@ -1,7 +1,7 @@
 class Carro {
-    String marca;
-    int velocidadeAtual;
-    int velocidadeMaxima;
+    private String marca;
+    private int velocidadeAtual;
+    private int velocidadeMaxima;
 
     Carro (){
         this.velocidadeAtual = 0;
@@ -14,19 +14,40 @@ class Carro {
     }
 
     public void acelerar (int velocidadeAcelerar){
+        
         if (velocidadeAtual < velocidadeMaxima && velocidadeAcelerar + velocidadeAtual < velocidadeMaxima){
             this.velocidadeAtual += velocidadeAcelerar;
             System.out.print("Nova velocidade cadastrada com sucesso!");
-
             return;
     } else if (velocidadeAtual >= velocidadeMaxima){
-        this.velocidadeAtual == velocidadeMaxima;
-        System.out.print ("A velocidade foi redefinida ao limite pois já atingiu/excedeu a margem!")
+        this.velocidadeAtual = velocidadeAtual;
+        System.out.print ("A velocidade foi redefinida ao limite pois já atingiu/excedeu a margem!");
+        return;
+    }
+        System.out.print("Operação inválida!");
+}
+
+        
+    public void frear (int velocidadeFrear){
+        if (velocidadeFrear < velocidadeAtual && velocidadeAtual - velocidadeFrear >= 0){
+            this.velocidadeAtual -= velocidadeFrear;
+            System.out.print ("A velocidade foi reduzida com sucesso!");
+            
+        } else if (velocidadeAtual < velocidadeFrear){
+            this.velocidadeAtual = velocidadeAtual;
+            System.out.print ("A velocidade foi mantida pois excedeu o limite definido!");
+
+        }
+        
+
+
+
+    }
+    public int getVelocidadeAtual(){
+        return this.velocidadeAtual;
+    }
+    public int getVelocidadeMaxima(){
+        return this.velocidadeMaxima;
     }
 
-    public void frear (int velocidadeFrear){
-        if (velocidadeFrear < velocidadeAtual && velocidadeAtual - velocidadeFrear > 0){
-            
-        }
-    }
 }
