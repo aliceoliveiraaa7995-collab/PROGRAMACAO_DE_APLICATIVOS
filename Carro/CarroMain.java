5951fbd7a285321c7f0c0e0c6b36f3db043e9fab
Alice Antunes <aliceoliveiraaa7995@gmail.com>
2026-03-06 16:39:29 -0300

    Update CarroMain.java

--- Carro/CarroMain.java
+++ Carro/CarroMain.java
@@ -3,9 +3,9 @@ class Main{
         Carro carros = new Carro( "xxxx", 30, 80);
 
         System.out.print(carros.getVelocidadeAtual());
-        carros.acelerar();
+        carros.acelerar(20);
         System.out.print(carros.getVelocidadeAtual());
-        carros.frear();
+        carros.frear(10);
         System.out.print(carros.getVelocidadeAtual());
     }
 }
