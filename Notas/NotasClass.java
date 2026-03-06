ab653af6bc6f83f0fcda489a8aa9ceb8c903ab4c
Alice Antunes <aliceoliveiraaa7995@gmail.com>
2026-03-06 16:51:01 -0300

    Create NotasClass.java

---
+++ Notas/NotasClass.java
@@ -0,0 +1,25 @@
+class Aluno {
+    private String nome;
+    private double [] notas;
+
+    public Aluno (String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNotas (double [] notas){
+
+    }
+
+    public double[] getNotas (){
+        return notas;
+    }
+
+    public double calcularMedia(){
+        double soma = 0;
+        
+    }
+}
