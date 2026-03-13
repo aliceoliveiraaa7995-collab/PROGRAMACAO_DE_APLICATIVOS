0188e89decbe1159d91a6e726c230ac240226e0c
Alice Antunes <aliceoliveiraaa7995@gmail.com>
2026-03-13 16:32:32 -0300

    Update PessoasMain.java

--- Pessoas/PessoasClass.java/PessoasMain.java
+++ Pessoas/PessoasClass.java/PessoasMain.java
@@ -1,1 +1,27 @@
+class Pessoa {
+    String nome;
+    int idade;
 
+    void exibirDados() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+    }
+}
+
+class Funcionario extends Pessoa {
+    String cargo;
+
+    void exibirDados() {
+        super.exibirDados();
+        System.out.println("Cargo: " + cargo);
+    }
+}
+
+class Cliente extends Pessoa {
+    String email;
+
+    void exibirDados() {
+        super.exibirDados();
+        System.out.println("Email: " + email);
+    }
+}
