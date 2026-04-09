f37c8d01308a0f7d7efcf240d4f25072872904bf
Alice Antunes <aliceoliveiraaa7995@gmail.com>
2026-04-09 14:23:53 -0300

    Create ClassIphone.java

---
+++ Pontuada/ClassIphone.java
@@ -0,0 +1,12 @@
+class Iphone {
+public void reproduzir(){
+   System.out.println("Reproduzindo...");
+}
+public void ligar(){
+   System.out.println("Ligando...");
+}
+public void navegar(){
+   System.out.println("Navegando...");
+}
+}
+
