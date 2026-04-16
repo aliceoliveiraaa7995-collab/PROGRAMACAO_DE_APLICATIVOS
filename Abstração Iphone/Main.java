346369d8eb26dd4fdbccebdbf4a9e228fa656edd
Alice Antunes <aliceoliveiraaa7995@gmail.com>
2026-04-16 16:14:51 -0300

    Create Main.java

---
+++ Abstração Iphone/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        iPhone iphone = new iPhone();
+
+        iphone.tocar();
+        iphone.selecionarMusica("Imagine");
+
+        iphone.ligar("99999-9999");
+        iphone.atender();
+
+        iphone.exibirPagina("google.com");
+        iphone.atualizarPagina();
+    }
+}
