8c8ebfbd86968451254de77b5ebdc54325c6afae
Alice Antunes <aliceoliveiraaa7995@gmail.com>
2026-04-16 16:10:06 -0300

    Update ClassProfessor.java

--- Humano/ClassProfessor.java
+++ Humano/ClassProfessor.java
@@ -4,8 +4,6 @@ public class Professor extends Pessoa {
     public void apresentar() {
         System.out.println("Sou um professor");
     }
-
-    // Sobrecarga
     public void apresentar(String mensagem) {
         System.out.println("Professor: " + mensagem);
     }
