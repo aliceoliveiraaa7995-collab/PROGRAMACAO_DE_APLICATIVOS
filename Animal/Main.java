d7204d13f5dc99c1bd2e8d94d2fe9a471c270a89
Alice Antunes <aliceoliveiraaa7995@gmail.com>
2026-04-16 14:30:56 -0300

    Create Main.java

---
+++ Animal/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[]args){
+    Cachorro c = new Cachorro();
+    Gato g = new Gato();
+    c.emitirSom();
+    c.emitirSom(2);
+    g.emitirSom;
+  }
+}
