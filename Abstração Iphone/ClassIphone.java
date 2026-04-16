247b6e41653b9642fe770d650ad080a879767e60
Alice Antunes <aliceoliveiraaa7995@gmail.com>
2026-04-16 16:14:30 -0300

    Create ClassIphone.java

---
+++ Abstração Iphone/ClassIphone.java
@@ -0,0 +1,38 @@
+public class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    public void tocar() {
+        System.out.println("Tocando música");
+    }
+
+    public void pausar() {
+        System.out.println("Música pausada");
+    }
+
+    public void selecionarMusica(String musica) {
+        System.out.println("Música selecionada: " + musica);
+    }
+
+    public void ligar(String numero) {
+        System.out.println("Ligando para: " + numero);
+    }
+
+    public void atender() {
+        System.out.println("Atendendo ligação");
+    }
+
+    public void iniciarCorreioVoz() {
+        System.out.println("Correio de voz iniciado");
+    }
+
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo: " + url);
+    }
+
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba aberta");
+    }
+
+    public void atualizarPagina() {
+        System.out.println("Página atualizada");
+    }
+}
