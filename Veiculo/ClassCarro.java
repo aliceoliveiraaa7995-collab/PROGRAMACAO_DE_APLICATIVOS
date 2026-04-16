69dfa8075771ede67f349ed42f093e40e26dcf0b
Alice Antunes <aliceoliveiraaa7995@gmail.com>
2026-04-16 15:41:23 -0300

    Create ClassCarro.java

---
+++ Veiculo/ClassCarro.java
@@ -0,0 +1,16 @@
+public class Carro extends Veiculo {
+
+    @Override
+    public void ligar() {
+        System.out.println("Carro ligado");
+    }
+
+    // Sobrecarga
+    public void ligar(boolean chavePresencial) {
+        if (chavePresencial) {
+            System.out.println("Carro ligado sem chave");
+        } else {
+            System.out.println("Carro ligado com chave");
+        }
+    }
+}
